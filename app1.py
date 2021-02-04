ffa10c9489c4a149350512f3c8a8007111b467e2
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2021-02-04 12:09:05 -0800

    Best match
    Recommending the best match if user enters a word with a typo and getting the confirmation.

--- app1.py
+++ app1.py
@@ -6,6 +6,10 @@ def translate(w):
     w = w.lower()
     if w in data:
         return(data[w])
+    elif len(get_close_matches(w, data.keys())) > 0:
+        answer = input("Did you mean %s instead? (y or n) " % get_close_matches(w, data.keys())[0])
+        if answer == 'y':
+            return(translate(get_close_matches(w, data.keys())[0]))
     else:
         return("Please try again. The word doesnt exist.")
 
