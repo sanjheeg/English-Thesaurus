79d810347f5178e5d72aacdd414ee67e7c931c60
sanjheeg <60531063+sanjheeg@users.noreply.github.com>
2021-02-03 23:46:40 -0800

    Non-existing words

--- app1.py
+++ app1.py
@@ -3,7 +3,11 @@ import json
 data = json.load(open("data.json"))
 
 def translate(w):
-    return(data[w])
+    if w in data:
+        return(data[w])
+    else:
+        return("Please try again. The word doesnt exist.")
+
 
 word = input("Enter a word: ")
 
